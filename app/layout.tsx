c5202e690f05fa5e63bb2eb53ce5db564359897e
AaryanNReal <aaryansharmawork@gmail.com>
2025-05-17 05:06:57 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@
       'frontend developer',
       'backend developer'
     ],
-    image: `${process.env.NEXT_PUBLIC_BASE_URL || ' '}/Me.jpg`, 
+    image: "/Me.jpg" ,
     siteName: 'Aaryan Sharma Portfolio',
   });
     return (
--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
     'frontend developer',
     'backend developer'
   ],
-  image: `${process.env.NEXT_PUBLIC_BASE_URL || ''}/Me.jpg`,
+  image: "/Me.jpg",
   siteName: 'Aaryan Sharma Portfolio',
 });
   return (
